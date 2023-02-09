ec9658729e55903bb3c32617c260d82ff6e7e4fc
Josip Tomo Licardo <licardo.josip@gmail.com>
2023-02-09 23:31:56 +0100

    Add PCPingRQ, PCPingRS

---
+++ PhobsRedisApi/XmlRpc/PCPingRQ.cs
@@ -0,0 +1,98 @@
+﻿namespace RedisAPI.XmlRpc
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class PCPingRQ
+    {
+
+        private PCPingRQAuth authField;
+
+        private string echoStringField;
+
+        /// <remarks/>
+        public PCPingRQAuth Auth
+        {
+            get
+            {
+                return this.authField;
+            }
+            set
+            {
+                this.authField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string EchoString
+        {
+            get
+            {
+                return this.echoStringField;
+            }
+            set
+            {
+                this.echoStringField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class PCPingRQAuth
+    {
+
+        private string usernameField;
+
+        private string passwordField;
+
+        private string siteIdField;
+
+        /// <remarks/>
+        public string Username
+        {
+            get
+            {
+                return this.usernameField;
+            }
+            set
+            {
+                this.usernameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Password
+        {
+            get
+            {
+                return this.passwordField;
+            }
+            set
+            {
+                this.passwordField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string SiteId
+        {
+            get
+            {
+                return this.siteIdField;
+            }
+            set
+            {
+                this.siteIdField = value;
+            }
+        }
+    }
+
+
+}
---
+++ PhobsRedisApi/XmlRpc/PCPingRS.cs
@@ -0,0 +1,68 @@
+﻿namespace RedisAPI.XmlRpc
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.phobs.net/phobs/webconnect/2018/")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.phobs.net/phobs/webconnect/2018/", IsNullable = false)]
+    public partial class PCPingRS
+    {
+        
+        private string echoStringField;
+
+        private PCPingRSResponseType responseTypeField;
+
+        /// <remarks/>
+        public string EchoString
+        {
+            get
+            {
+                return this.echoStringField;
+            }
+            set
+            {
+                this.echoStringField = value;
+            }
+        }
+
+        /// <remarks/>
+        public PCPingRSResponseType ResponseType
+        {
+            get
+            {
+                return this.responseTypeField;
+            }
+            set
+            {
+                this.responseTypeField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.phobs.net/phobs/webconnect/2018/")]
+    public partial class PCPingRSResponseType
+    {
+
+        private object successField;
+
+        /// <remarks/>
+        public object Success
+        {
+            get
+            {
+                return this.successField;
+            }
+            set
+            {
+                this.successField = value;
+            }
+        }
+    }
+
+
+}
