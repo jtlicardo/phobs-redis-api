b1d986290af319a24acc28c648ea423c074997ac
Josip Tomo Licardo <licardo.josip@gmail.com>
2023-02-28 23:55:51 +0100

    Add unit tests for PropertyData classes

---
+++ PhobsRedisApi.UnitTests/Controllers/TestPropertyDataController.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PhobsRedisApi.Controllers;
+using PhobsRedisApi.Dtos;
+using PhobsRedisApi.Models;
+using PhobsRedisApi.Services.PropertyData;
+
+namespace PhobsRedisApi.UnitTests.Controllers
+{
+    [TestFixture]
+    public class TestPropertyDataController
+    {
+        private Mock<IPropertyDataService> _mockService;
+        private PropertyDataController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockService = new Mock<IPropertyDataService>();
+            _controller = new PropertyDataController(_mockService.Object);
+        }
+
+        [Test]
+        public async Task GetPropertyData_ReturnsOkObjectResult_WithValidDto()
+        {
+            // Arrange
+            GetPropertyDataDto request = new GetPropertyDataDto
+            {
+                Property = "XXXX", Adults = 2, Chd = 1, Pets = 0,
+                Rate = "ABC", Date = "yyyyMMdd", Nights = 5
+            };
+            PropertyDataRS response = new PropertyDataRS
+            {
+                PropertyId = "XXXX", MinPricePerDay = 123.12f, Availability = true
+            };
+            _mockService.Setup(s => s.GetPropertyData(request)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetPropertyData(request);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            Assert.IsInstanceOf<PropertyDataRS>(((OkObjectResult)result.Result).Value);
+        }
+    }
+}
---
+++ PhobsRedisApi.UnitTests/Services/TestPropertyDataService.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using PhobsRedisApi.Data;
+using PhobsRedisApi.Dtos;
+using PhobsRedisApi.Models;
+using PhobsRedisApi.Services.PropertyData;
+
+namespace PhobsRedisApi.UnitTests.Services
+{
+    [TestFixture]
+    public class TestPropertyDataService
+    {
+        private PropertyDataService _propertyDataService;
+        private Mock<IDataRepo> _repoMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repoMock = new Mock<IDataRepo>();
+            _propertyDataService = new PropertyDataService(_repoMock.Object);
+        }
+
+        [Test]
+        public async Task GetPropertyData_ReturnsResponseObject_WithValidDto()
+        {
+            // Arrange
+            var request = new GetPropertyDataDto
+            {
+                Property = "XXXXX", Adults = 2, Chd = 1, Pets = 0,
+                Rate = "XXX", Date = "20240605", Nights = 1
+            };
+
+            _repoMock
+                .Setup(x => x.GetData(It.IsAny<string>()))
+                .Returns("100");
+
+            // Act
+            var result = await _propertyDataService.GetPropertyData(request);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<PropertyDataRS>());
+        }
+    }
+}
