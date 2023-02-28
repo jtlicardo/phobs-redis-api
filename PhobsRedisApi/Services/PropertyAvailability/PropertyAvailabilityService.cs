c6cbd62952f041eabeb02e4f1923705e7295f8ea
Josip Tomo Licardo <licardo.josip@gmail.com>
2023-02-28 21:51:24 +0100

    Saving min unit price per night

--- PhobsRedisApi/Services/PropertyAvailability/PropertyAvailabilityService.cs
+++ PhobsRedisApi/Services/PropertyAvailability/PropertyAvailabilityService.cs
@@ -60,44 +60,25 @@ namespace PhobsRedisApi.Services.PropertyAvailability
             {
                 foreach (var rate in property.RatePlans)
                 {
+                    decimal minUnitPricePerNight = rate.Units[0].Rate.Price.Value;
+
                     foreach (var unit in rate.Units)
                     {
-                        if (unit.Rate.PriceBreakdown is null) return;
-                        decimal[] prices = new decimal[req.Nights]; // Prices for each day
-                        int i = 0; // Counter used for prices array
-                        foreach (var day in unit.Rate.PriceBreakdown)
-                        {
-                            prices[i++] = day.Price.Value;
-                            // Example key: PHDIA:3:1:0:WHB:JRSUP:20240426
-                            string date = day.Date.ToString("yyyyMMdd");
-                            string key =
-                                $"{property.PropertyId}:" +
-                                $"{req.Adults}:" +
-                                $"{req.ChdGroup1}:" +
-                                $"{req.Pets}:" +
-                                $"{rate.RateId}:" +
-                                $"{unit.UnitId}:" +
-                                $"{date}";
-                            string price = day.Price.Value.ToString();
-                            _repo.SaveData(key, price); // Saving individual prices
-                        }
-                        // Example key: PHDIA:3:1:0:WHB:JRSUP:20240426:3
-                        string pricesDate = req.Date.Replace("-", "");
-                        string pricesKey =
-                            $"{property.PropertyId}:" +
-                            $"{req.Adults}:" +
-                            $"{req.ChdGroup1}:" +
-                            $"{req.Pets}:" +
-                            $"{rate.RateId}:" +
-                            $"{unit.UnitId}:" +
-                            $"{pricesDate}:" +
-                            $"{req.Nights}";
-                        foreach (var price in prices)
-                        {
-                            _repo.PushToList(pricesKey, price.ToString()); // Saving prices array
-                        }
-                        _repo.SetExpiration(pricesKey, TimeSpan.FromSeconds(30));
+                        if (unit.Rate.Price.Value < minUnitPricePerNight)
+                            minUnitPricePerNight = unit.Rate.Price.Value;
                     }
+
+                    string date = req.Date.Replace("-", "");
+                    string key =
+                        $"{property.PropertyId}:" +
+                        $"{req.Adults}:" +
+                        $"{req.ChdGroup1}:" +
+                        $"{req.Pets}:" +
+                        $"{rate.RateId}:" +
+                        $"{date}:" +
+                        $"{req.Nights}";
+
+                    _repo.SaveData(key, minUnitPricePerNight.ToString());
                 }
             }
         }
